4d1a20e1c1251f8cd2ae67ec8fa1f79c152a32e1
Munther20011 <munthermoufeed@gmail.com>
2025-10-09 12:33:14 +0300

    Week4 part1

--- week3/app/src/main/java/msku/ceng/mobile/week3/MainActivity.java
+++ week3/app/src/main/java/msku/ceng/mobile/week3/MainActivity.java
@@ -1,9 +1,13 @@
 package msku.ceng.mobile.week3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,9 +17,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    static final int FINAL_REQUEST = 1;
 
     ListView listView;
     List<Post> postList = new ArrayList<>();
+
+    Button btnpost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,29 @@ public class MainActivity extends AppCompatActivity {
         PostAdapter postAdapter = new PostAdapter(this , postList);
         listView.setAdapter(postAdapter);
 
+        btnpost.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivityForResult(intent, FINAL_REQUEST);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == FINAL_REQUEST && requestCode == PostActivity.RESULT_OK){
+            Post post = new Post();
+            post.setMessage(data.getCharSequenceExtra("nsg").toString());
+            post.setImage(data.getParcelableExtra("bitmap"));
+            postList.add(post);
+            ((PostAdapter)listView.getAdapter()).notifyDataSetChanged();
+
+        }
 
     }
 }
